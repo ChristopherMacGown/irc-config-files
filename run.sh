31c30bae2a5865ae63e7ae46aa6701c1746cefcd
Christopher MacGown <ignoti+github@gmail.com>
2017-05-15 08:50:15 -0700

    Add tmux irssi runner

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tmux new -d -s irc 'docker run -it \
+			-e TERM -u $(id -u):$(id -g) \
+			--log-driver=none \
+			-v irclogs:/home/user/irclogs \
+			-v $HOME/.irssi:/home/user/.irssi:ro \
+			-v /etc/localtime:/etc/localtime:ro \
+			irssi'
