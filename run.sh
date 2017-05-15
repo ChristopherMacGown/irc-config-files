d864b7bb81c1ca8c1077206e69bb3b37b614b586
Christopher MacGown <ignoti+github@gmail.com>
2017-05-15 10:05:46 -0700

    Correct bind mount, remove old irssi config variable

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 tmux new -d -s irc 'docker run -it \
 			-e TERM -u $(id -u):$(id -g) \
 			--log-driver=none \
-			-v irclogs:/home/user/irclogs \
-			-v $HOME/.irssi:/home/user/.irssi:ro \
+			-v $HOME/irclogs:/home/user/irclogs \
+			-v $HOME/.irssi:/home/user/.irssi \
 			-v /etc/localtime:/etc/localtime:ro \
 			jess/irssi'
